7d20712b11bc03e6cb29cae93bc32aa34718e023
Palash shah <113499117+shahpalash10@users.noreply.github.com>
2023-11-10 13:58:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
-    }, 1200);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
