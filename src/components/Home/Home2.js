c372298072efa72699dffc9816f40eb613735aff
Palash shah <113499117+shahpalash10@users.noreply.github.com>
2024-08-19 13:04:29 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,7 +16,7 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              IDHAR<span className="purple"> SUNO </span>
+              HI<span className="purple"> THERE </span>
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
